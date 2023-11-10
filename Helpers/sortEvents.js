106b1d398c21dd68c59d55ec5cb1dbb193f4f59a
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2023-11-11 00:15:56 +0530

    Another Fix on Sorting of Events

--- Helpers/sortEvents.js
+++ Helpers/sortEvents.js
@@ -1,8 +1,10 @@
 
 
 function compareEvents(eventA, eventB) {
-    const dateAString = eventA?.date?.substring(0,eventA?.date?.indexOf("T"))+"T"+eventA?.time+":00.000Z"
-    const dateBString = eventB?.date?.substring(0,eventB?.date?.indexOf("T"))+"T"+eventB?.time+":00.000Z"
+    const dateAStringFormat = eventA?.date?.toString();
+    const dateBStringFormat = eventB?.date?.toString();
+    const dateAString = dateAStringFormat.substring(0,dateAStringFormat.indexOf("T"))+"T"+eventA?.time+":00.000Z"
+    const dateBString = dateBStringFormat.substring(0,dateBStringFormat.indexOf("T"))+"T"+eventB?.time+":00.000Z"
 
     const dateA = new Date(dateAString);
     const dateB = new Date(dateBString);
