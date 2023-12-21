c3f1b4796013a2ed2206766d1a1d4a48fd9e2971
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2023-12-21 22:14:50 +0530

    FIX: Sorting Comparator

--- Helpers/sortEvents.js
+++ Helpers/sortEvents.js
@@ -1,15 +1,9 @@
 
 
 function compareEvents(eventA, eventB) {
-    const dateAStringFormat = eventA?.date?.toString();
-    const dateBStringFormat = eventB?.date?.toString();
-    const dateAString = dateAStringFormat.substring(0,dateAStringFormat.indexOf("T"))+"T"+eventA?.time+":00.000Z"
-    const dateBString = dateBStringFormat.substring(0,dateBStringFormat.indexOf("T"))+"T"+eventB?.time+":00.000Z"
-
-    const dateA = new Date(dateAString);
-    const dateB = new Date(dateBString);
-  
-    return dateB - dateA ;
+    const dateA = new Date(eventA.date);
+    const dateB = new Date(eventB.date);
+    return dateA - dateB;
 }
 
 module.exports = compareEvents; 
