ce8c71c5d9c38c50435f4409cea91d050f1d1f7c
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2023-12-21 22:21:25 +0530

    Reversed Sorting Order

--- Helpers/sortEvents.js
+++ Helpers/sortEvents.js
@@ -3,7 +3,7 @@
 function compareEvents(eventA, eventB) {
     const dateA = new Date(eventA.date);
     const dateB = new Date(eventB.date);
-    return dateA - dateB;
+    return dateB - dateA;
 }
 
 module.exports = compareEvents; 
